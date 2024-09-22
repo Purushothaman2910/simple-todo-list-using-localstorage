7debeed23abcbbfa6ce5bc03f14af0fe6e6acc1b
Purushothaman2910 <velusamypurushothaman@gmail.com>
2024-09-22 22:50:23 +0530

    new changes are done

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,74 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import {TodoProvider} from './context/index'
-import {TodoForm, TodoItem} from './components/index'
+import React, { useEffect, useState } from 'react';
+import { TodoProvider } from './context/index';
+import { TodoForm, TodoItem } from './components/index';
 
 function TodoList() {
-  let [todos , setTodoList] = useState([])  
+  const [todos, setTodoList] = useState([]);
 
-  function addTodo(todo){
-    setTodoList((preValue) => {
-      console.log(todo);      
-      return [        
-        ...preValue ,
+  function addTodo(todo) {
+    setTodoList((prevValue) => {
+      return [
+        ...prevValue,
         {
-          id : Date.now() ,
-          ...todo
-        } 
-      ]
-    })
+          id: Date.now(),
+          ...todo,
+        },
+      ];
+    });
   }
 
-  function deleteTodo(id){
+  function deleteTodo(id) {
+    setTodoList((prevValue) => prevValue.filter((item) => item.id !== id));
     setTodoList((preValue) => preValue.filter(item => item.id !== id))
   }
 
-  function updateTodo(id , todo){    
-    setTodoList((preValue) => preValue.map((item) => (item.id === id) ? {id:id , ...todo } : item))
+  function updateTodo(id, updatedTodo) {
+    setTodoList((prevValue) =>
+      prevValue.map((item) =>
+        item.id === id ? { ...item, ...updatedTodo } : item
+      )
+    );
   }
 
-  function toggleComplete(id){
-    setTodoList((preValue) => {
-      return preValue.map((todo) => {
-        if(todo.id === id){
-          return {...todo , completed : !todo.completed}
-        }else{
-          return todo
-        }
-      })
-    })
+  function toggleComplete(id) {
+    setTodoList((prevValue) => {
+      return prevValue.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      );
+    });
   }
 
-  function getTodo(){
-    let todo = JSON.parse(localStorage.getItem('todos'))   
-    if(todo && todo.length){
-      setTodoList(todo)   
-    }    
+  function getTodo() {
+    const storedTodos = JSON.parse(localStorage.getItem('todos')) ;
+    if (storedTodos.length > 0 && storedTodos) {
+      setTodoList(storedTodos);
+    }
   }
 
-  useEffect(()=>{
-    getTodo()
-  } ,[])
+  useEffect(() => {
+    getTodo();
+  }, []);
 
-  useEffect(()=>{   
-   localStorage.setItem('todos' , JSON.stringify(todos)) 
-  } , [todos])
-
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   return (
-    <TodoProvider value={{ todos  ,addTodo ,deleteTodo ,toggleComplete ,updateTodo }}>      
+    <TodoProvider value={{ todos, addTodo, deleteTodo, toggleComplete, updateTodo }}>
       <div className='w-full h-screen font-mono flex flex-col items-center p-9'>
-          <p className='text-2xl font-bold uppercase'> my todo list </p>
-          <TodoForm />
-          <div className='flex flex-col gap-3'>
-          {
-            todos.map((item , index) => (
-              <TodoItem key={index} todo={item} />
-            ))
-          }
-          </div>
+        <p className='text-2xl font-bold uppercase'> My Todo List </p>
+        <TodoForm />
+        <div className='flex flex-col gap-3'>
+          {todos.map((item) => (
+            <TodoItem key={item.id} todo={item} />
+          ))}
+        </div>
       </div>
     </TodoProvider>
-  )
+  );
 }
 
-export default TodoList
+export default TodoList;
